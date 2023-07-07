43e584fa75e4d3011d7a0e9183b2adcd13817a8b
jm0rt1 <50722516+jm0rt1@users.noreply.github.com>
2023-07-06 21:12:44 -0400

    Initial commit

---
+++ start.py
@@ -0,0 +1,25 @@
+import os
+
+filename = "structure.txt"
+# change this if you want to create files in a specific directory
+base_path = os.getcwd()
+
+prev_name = []
+with open(filename, 'r') as file:
+    for line in file:
+        indent_count = len(line) - len(line.lstrip())
+        level = indent_count // 4  # Assuming an indentation of 4 spaces.
+        name = line.strip()
+        name = name.replace('/', '')
+
+        # Adjust the current path according to the indentation level
+
+        prev_name = prev_name[:level-1] + [name]
+
+        full_path = os.path.join(base_path, *prev_name)
+
+        if '/' in line:
+            os.makedirs(full_path, exist_ok=True)
+        else:
+            if not os.path.exists(full_path):
+                open(full_path, 'a').close()
